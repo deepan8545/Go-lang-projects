3e9ec8cd56f3323cfecd1f151c6060ce5385c7dd
deepan8545 <deepanakash123@gmail.com>
2024-08-06 20:41:06 -0500

    Learning scope

---
+++ Scopes/main.go
@@ -0,0 +1,16 @@
+package main
+
+// file scope
+import "fmt"
+
+// package scope
+const ok = true
+
+// package scope
+func main() { // block scope starts
+
+	var hello = "Hello"
+	//hello and ok are visible here
+	fmt.Println(hello, ok)
+
+}
